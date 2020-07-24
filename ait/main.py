f7f81e364b8a49403a27d76b9b4d0e6492457f8e
Abdellatif-AitHammadi <abdellatif.aithammadi@gmail.com>
2020-07-24 18:39:31 +0100

    +++

--- ait/main.py
+++ ait/main.py
@@ -11,15 +11,15 @@ content = open("lnkdhtml.html").read()#driver.page_source
 soup = BeautifulSoup(content)
 r=soup.findAll('div', attrs={'class':class_pub})
 
-T=[]
-for a in soup.findAll('div',href=True, attrs={'class':class_pub}):
-	name=a.find('span', attrs={'class':class_text})
-	print(name)
-	# price=a.find('div', attrs={'class':'_1vC4OE _2rQ-NK'})
-	# rating=a.find('div', attrs={'class':'hGSR34 _2beYZw'})
-	T.append(name.text)
-	# prices.append(price.text)
-	# ratings.append(rating.text) 
+# T=[]
+# for a in soup.findAll('div',href=True, attrs={'class':class_pub}):
+# 	name=a.find('span', attrs={'class':class_text})
+# 	print(name)
+# 	# price=a.find('div', attrs={'class':'_1vC4OE _2rQ-NK'})
+# 	# rating=a.find('div', attrs={'class':'hGSR34 _2beYZw'})
+# 	T.append(name.text)
+# 	# prices.append(price.text)
+# 	# ratings.append(rating.text) 
 
 
 # df = pd.DataFrame({'ok1':T}) 
