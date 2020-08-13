5170275cd6d4fc492a6a3c605ea7bdfac2c95428
Abdellatif-AitHammadi <abdellatif.aithammadi@gmail.com>
2020-08-13 20:41:11 +0100

    ++++

--- hello/views.py
+++ hello/views.py
@@ -6,14 +6,5 @@ from .models import Greeting
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
-    return render(request, "index.html")
+    return render(request, "choose.html")
 
-
-def db(request):
-
-    greeting = Greeting()
-    greeting.save()
-
-    greetings = Greeting.objects.all()
-
-    return render(request, "db.html", {"greetings": greetings})
