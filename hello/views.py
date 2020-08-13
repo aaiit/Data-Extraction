34731de626685414df27fdf82c4dcf5f6d0b8a50
Abdellatif-AitHammadi <abdellatif.aithammadi@gmail.com>
2020-08-13 21:55:01 +0100

    +++

--- hello/views.py
+++ hello/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import json
 from .models import Greeting
 
 # Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, "choose.html")
+	# return HttpResponse('Hello from Python!')
+	return render(request, "choose.html")
 
 
 def form(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, "f.html")
+	# return HttpResponse('Hello from Python!')
+	if request.method=='POST':
+		data=json.loads(request.body)
+		return HttpResponse("You send data")
+	return render(request, "f.html")
 
 
 def standardOperators(request):
