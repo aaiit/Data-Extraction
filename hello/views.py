9fab6f0a5d150b1cdccf773de594c6a9f89d4dec
Abdellatif-AitHammadi <abdellatif.aithammadi@gmail.com>
2020-08-13 22:04:25 +0100

    +++

--- hello/views.py
+++ hello/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import json
 from .models import Greeting
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
 def index(request):
 	# return HttpResponse('Hello from Python!')
 	return render(request, "choose.html")
 
-
+@csrf_exempt 
 def form(request):
 	# return HttpResponse('Hello from Python!')
 	if request.method=='POST':
