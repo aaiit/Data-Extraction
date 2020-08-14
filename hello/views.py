4657d37d458508f788c9f24075454e26a26dfb32
Abdellatif-AitHammadi <abdellatif.aithammadi@gmail.com>
2020-08-14 17:41:22 +0100

    ++++

--- hello/views.py
+++ hello/views.py
@@ -18,6 +18,28 @@ def formText(request):
 		#fields = {'q': 'covid19', 'lang': 'en', 'result_type': 'popular',"len":2}
 		return HttpResponse(test(fields))
 	return render(request, "form1.html")
+
+ZIPFILE_NAME = 'Imagesdownl.zip'
+
+
+def download(request):
+    """Download archive zip file of code snippets"""
+    response = HttpResponse(content_type='application/zip')
+    zf = zipfile.ZipFile(response, 'w')
+
+    # create the zipfile in memory using writestr
+    # add a readme
+    zf.writestr(README_NAME, README_CONTENT)
+
+    # retrieve snippets from ORM and them to zipfile
+    scripts = Script.objects.all()
+    for snippet in scripts:
+        zf.writestr(snippet.name, snippet.code)
+
+    # return as zipfile
+    response['Content-Disposition'] = f'attachment; filename={ZIPFILE_NAME}'
+    print("done----<<>")
+    return response
 @csrf_exempt 
 def formImage(request):
 	# return HttpResponse('Hello from Python!')
@@ -25,11 +47,7 @@ def formImage(request):
 		fields=json.loads(request.body)
 		print("dow--->>>")
 		downloadImages(fields)
-		zip_file = open("Imagesdownl.zip", 'r')
-		response = HttpResponse(zip_file, content_type='application/force-download')
-		response['Content-Disposition'] = 'attachment; filename="%s"' % 'Imagesdownl.zip'
-		print("done----<<>")
-		return response
+		return download(request)
 		
 	return render(request, "fimage.html")
 
