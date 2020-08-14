d38e8d144a3cc8c4950ac0bdfe78712c896cc73a
AAyyoouubb <th3assis@gmail.com>
2020-08-14 11:44:26 +0000

    sssss

--- Core.py
+++ Core.py
@@ -21,9 +21,26 @@ t = Twitter(consumer_key, consumer_secret)
 # trends=t.get_available_places_with_trends()
 # print(json.dumps(sorted(trends,key=lambda x: x['tweet_volume'] if x['tweet_volume'] else 1,reverse=True),indent=4))
 
+def flatten_json(y):
+    out = {}
+
+    def flatten(x, name=''):
+        if type(x) is dict:
+            for a in x:
+                flatten(x[a], name + a + '_')
+        elif type(x) is list:
+            i = 0
+            for a in x:
+                flatten(a, name + str(i) + '_')
+                i += 1
+        else:
+            out[name[:-1]] = x
+
+    flatten(y)
+    return out
+
 def test(fields):
 	# fields = {'q': 'covid19', 'lang': 'en', 'result_type': 'popular'}
 	type=fields.pop('type','json')
 	tweets = t.search_tweets_images(fields.pop('len',10), fields, TWEET_IMAGES_KEYS)
-	return json.dumps(tweets, default=str, indent=4)
-
+	return json.dumps(tweets, default=str, indent=4) if type=='json' else flatten_json(tweets)
