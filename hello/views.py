502eac72edba50d968d696797b3a954673ccd99d
Abdellatif-AitHammadi <abdellatif.aithammadi@gmail.com>
2020-08-14 18:16:07 +0100

    +++

--- hello/views.py
+++ hello/views.py
@@ -3,9 +3,9 @@ from django.http import HttpResponse
 import json
 from .models import Greeting
 from django.views.decorators.csrf import csrf_exempt
-
 from Core import *
 from Sabah import downloadImages
+import zipfile
 # Create your views here.
 def index(request):
 	return render(request, "choose.html")
@@ -19,35 +19,18 @@ def formText(request):
 		return HttpResponse(test(fields))
 	return render(request, "form1.html")
 
-ZIPFILE_NAME = 'Imagesdownl.zip'
 
-
-def download(request):
-    """Download archive zip file of code snippets"""
-    response = HttpResponse(content_type='application/zip')
-    zf = zipfile.ZipFile(response, 'w')
-
-    # create the zipfile in memory using writestr
-    # add a readme
-    zf.writestr(README_NAME, README_CONTENT)
-
-    # retrieve snippets from ORM and them to zipfile
-    scripts = Script.objects.all()
-    for snippet in scripts:
-        zf.writestr(snippet.name, snippet.code)
-
-    # return as zipfile
-    response['Content-Disposition'] = f'attachment; filename={ZIPFILE_NAME}'
-    print("done----<<>")
-    return response
 @csrf_exempt 
 def formImage(request):
 	# return HttpResponse('Hello from Python!')
 	if request.method=='POST':
 		fields=json.loads(request.body)
-		print("dow--->>>")
-		downloadImages(fields)
-		return download(request)
+		# downloadImages(fields)
+		path_to_file="downloadImages.zip"
+		zip_file = open(path_to_file, 'r')
+		response = HttpResponse(zip_file, content_type='application/force-download')
+		response['Content-Disposition'] = 'attachment; filename="%s"' % 'foo.zip'
+		return response
 		
 	return render(request, "fimage.html")
 
