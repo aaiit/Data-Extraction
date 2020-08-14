c412f0649d0d32bbc4adbb943da509c33419e2e6
Abdellatif-AitHammadi <abdellatif.aithammadi@gmail.com>
2020-08-14 17:24:25 +0100

    ++++

--- hello/views.py
+++ hello/views.py
@@ -5,6 +5,7 @@ from .models import Greeting
 from django.views.decorators.csrf import csrf_exempt
 
 from Core import *
+from Sabah import downloadImages
 # Create your views here.
 def index(request):
 	return render(request, "choose.html")
@@ -22,8 +23,12 @@ def formImage(request):
 	# return HttpResponse('Hello from Python!')
 	if request.method=='POST':
 		fields=json.loads(request.body)
-		#fields = {'q': 'covid19', 'lang': 'en', 'result_type': 'popular',"len":2}
-		return HttpResponse(test(fields))
+		downloadImages(fields)
+		zip_file = open(path_to_file, 'r')
+		response = HttpResponse(zip_file, content_type='application/force-download')
+		response['Content-Disposition'] = 'attachment; filename="%s"' % 'Imagesdownl.zip'
+		return response
+		
 	return render(request, "fimage.html")
 
 def formVideo(request):
