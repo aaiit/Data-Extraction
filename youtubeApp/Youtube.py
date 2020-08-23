e9c193eb4aba174e7783bd23189f7cbf75784a8a
AAyyoouubb <42122470+AAyyoouubb@users.noreply.github.com>
2020-08-23 20:06:21 +0000

    me

---
+++ youtubeApp/Youtube.py
@@ -0,0 +1,84 @@
+import json
+
+import pandas
+
+from Youtube.YoutubeSearchByKeywords import SearchRequest
+from Youtube.YoutubeVideoComments import VideoComments
+from Youtube.YoutubeVideoInformations import VideoContent
+
+api = 'AIzaSyCPYehigrYNp7TkN12zTmwu3cDRt2CM4ZE'
+
+search = SearchRequest(api)
+video_infos = VideoContent(api).filter()
+comments = VideoComments(api).filter()
+
+
+# fields_search = {'q': 'vaccine corona', 'max_results': '20', 'by_channel': '', 'order_by': 'relevance', 'related_to': '',
+#           'before_time': '2020-30-07T00:00:00Z', 'after_time': '2020-22-08T00:00:00Z'}
+
+# fields_comments = {'sort': 'relevance', 'count': '30', 'format': True, 'video_id': '32161321613',
+#           'keywords': 'great vaccine'}
+
+
+def search_videos(fields):
+    count = fields.pop('max_results', 50)
+    search.type('video') \
+        .location(fields.pop('lat', None), fields.pop('long', None), fields.pop('radius', None)) \
+        .max_results(count if count <= 50 else 50) \
+        .by_channel(fields.pop('by_channel', '')) \
+        .order_by(fields.pop('order_by', '')) \
+        .related_to(fields.pop('related_to', '')) \
+        .before_time(fields.pop('before_time', None)) \
+        .after_time(fields.pop('after_time', None)) \
+        .filter_snippet(fields.pop('filter_snippet', True)) \
+        .region(fields.pop('region', 'MA'))
+    videos = search.request()
+    while count > 50 and search.nextPage:
+        count -= 50
+        search.max_results(count if count <= 50 else 50)
+        search.nextPage()
+        videos.extend(search.request())
+    if not videos:
+        return []
+    i = 0
+    while i < len(videos):
+        vid = videos[i]
+        # Try to get more informations about the video as well as its comments.
+        video_id = vid['id']['videoId']
+
+        # Add the new informations to the list of the videos.
+        if vid['id']['kind'] == 'video':
+            video = video_infos.request(video_id)
+            if not video:
+                # In case of any error remove this video from the results and go next.
+                videos.remove(vid)
+                continue
+            add_video_newInfos(vid, video)
+        i += 1
+    return json.dumps(videos, indent=3, default=str) if fields.pop('type', 'json') else pandas.DataFrame(videos).to_csv(
+        index=False)
+
+
+def search_comments(fields):
+    count = fields.pop('count', 50)
+    comments.max_comments(count if count <= 50 else 50) \
+        .format(fields.pop('format', True)) \
+        .sort(fields.pop('sort', '')) \
+        .related_to_video(fields.pop('video_id', '')) \
+        .related_to_keywords(fields.pop('keywords', ''))
+    results = comments.request()
+    while count > 50 and comments.nextPage:
+        count -= 50
+        comments.max_comments(count if count <= 50 else 50)
+        comments.nextPage()
+        results.extend(comments.request())
+    return json.dumps(results, indent=3, default=str) if fields.pop('type', 'json') else pandas.DataFrame(
+        results).to_csv(index=False)
+
+
+##################################################################  HELPER FUNCTION
+def add_video_newInfos(old, new):
+    # Beware of not having a key in the returned data.
+    old['snippet'].update(new['snippet'] if 'snippet' in new else {})
+    old['statistics'] = new['statistics'] if 'statistics' in new else {}
+    old['topicCategories'] = new['topicCategories'] if 'topicCategories' in new else {}
---
+++ youtubeApp/YoutubeSearchByKeywords.py
@@ -0,0 +1,177 @@
+import json
+from time import sleep
+
+import requests as req
+
+from Youtube.YoutubeVideoInformations import VideoContent
+
+
+
+class SearchRequest:
+    def __init__(self, api):
+        self.nextPage = None
+        self.prevPage = None
+        self.link = 'https://www.googleapis.com/youtube/v3/search?part=snippet'
+        self.api = '&key=' + api
+        self.fields = {}
+
+    def type(self, types=None):
+        '''
+        :param types: a string containing any combination of these separated by commas:
+                        ['video','channel','playlist']
+        '''
+        # check validity of types variable.
+        if types is not None:
+            types = types.lower()
+            if not {x.strip() for x in types.split(',')}.issubset({'channel', 'video', 'playlist'}):
+                raise ValueError('The value of "types" isn\'t valid.')
+            self.fields['&type='] = '&type='.join([x.strip() for x in types.split(',')])
+        else:
+            self.fields.pop('&type=', None)
+        return self
+
+    def location(self, lat=None, long=None, radius=None):
+        '''
+        :param lat: 10.3333
+        :param long: -12.0004
+        :param radius: cannot exceed 1000 km. You can adjust the unit: mi,km,ft,m. (e.g: 0.5mi)
+                ----> radius Is mandatory.
+        '''
+        if lat is None:
+            self.fields.pop('&location=', None)
+        else:
+            self.fields['&location='] = lat + '%2C' + long + '&locationRadius=' + radius + 'mi'
+        return self
+
+    def query(self, q=''):
+        '''
+        :param q: list of keywords to search by.
+                -----> can include | (pipe) to search for results associated with either parts. (e.g: nike|adidas)
+                            ----->Should encode it in url using %7C.
+                -----> can include - (dash) to exclude the following part from the results. (e.g: nike-adidas puma)
+                            exclude any result related to adidas or puma.
+        '''
+        if q == '':
+            self.fields.pop('&q=', None)
+        else:
+            self.fields['&q='] = q
+        return self
+
+    def max_results(self, max=0):
+        '''
+        :param max: the number of results to return in each page( per request).
+        '''
+        if max == 0:
+            self.fields.pop('&maxResults=', None)
+        else:
+            self.fields['&maxResults='] = str(max)
+        return self
+
+    def order_by(self, order=''):
+        '''
+        :param order:['date','rating','relevance','title','videoCount','viewCount']
+                ----> all criterias are sorted from better to worst.
+                ----> videoCount: sort channel based on the number of the uploaded videos.
+        '''
+        if order == '':
+            self.fields.pop('&order=', None)
+        else:
+            self.fields['&order='] = order
+        return self
+
+    def by_channel(self, channel=''):
+        '''
+        :param channel: id of the channel that you want to search inside of it.
+        '''
+        if channel == '':
+            self.fields.pop('&channelId=', None)
+        else:
+            self.fields['&channelId='] = channel
+        return self
+
+    def related_to(self, video_id=''):
+        '''
+        :param video_id: id of a video
+        '''
+        if video_id == '':
+            self.fields.pop('&relatedToVideoId=', None)
+        else:
+            self.fields['&relatedToVideoId='] = video_id
+        return self
+
+    def region(self, region=''):
+        '''
+        :param region:  ISO 3166-1 alpha-2 country code: MA,US,UK ...
+        '''
+        if region == '':
+            self.fields.pop('&regionCode=', None)
+        else:
+            self.fields['&regionCode='] = region
+        return self
+
+    def next_page(self):
+        if self.nextPage is None:
+            self.fields.pop('&pageToken=', None)
+        else:
+            self.fields['&pageToken='] = self.nextPage
+        return self
+
+    def prev_page(self):
+        if self.prevPage is None:
+            self.fields.pop('&pageToken=', None)
+        else:
+            self.fields['&pageToken='] = self.prevPage
+        return self
+
+    def after_time(self, date=None):
+        # date variable should be like this: yyyy-dd-mmThh:mm:ssZ
+        if date is None:
+            self.fields.pop('&publishedAfter=', None)
+        else:
+            self.fields['&publishedAfter='] = str(date)
+        return self
+
+    def before_time(self, date=None):
+        # date variable should be like this: yyyy-dd-mmThh:mm:ssZ
+        if date is None:
+            self.fields.pop('&publishedBefore=', None)
+        else:
+            self.fields['&publishedBefore='] = str(date)
+        return self
+
+    def filter_snippet(self, snippet=True):
+        '''
+        :param snippet: True if you want to include only:
+                 nextPageToken,prevPageToken,items(id,snippet(publishedAt,channelId,title))
+                 in your results
+        '''
+        if not snippet:
+            self.fields.pop('&fields=', None)
+        else:
+            self.fields['&fields='] = \
+                'nextPageToken,prevPageToken,' \
+                'items(id,snippet(publishedAt,channelId,title))'
+        return self
+
+    def request(self):
+        """
+        The function request data from youtube using the query built
+        :return: a list containing the information about the results;
+                the results are a list of dictionaries.
+                ---> In case of error, the list is going to be empty.
+        """
+        link = self.link + self.api + ''.join([key + val for key, val in self.fields.items()])
+        res = req.get(link)
+        print('Videos Retrieval: ', res)
+        if res.status_code != 200:
+            raise ValueError("The request wasn't done: ", res.status_code)
+        data = json.loads(res.content)
+        self.nextPage = data['nextPageToken'] if 'nextPageToken' in data else None
+        self.prevPage = data['prevPageToken'] if 'prevPageToken' in data else None
+        if 'items' in data and len(data['items']) > 0:
+            data = data['items']
+            for i in range(len(data)):
+                data[i]['id']['kind'] = data[i]['id']['kind'].split('#')[-1]
+        else:
+            data = []
+        return data
