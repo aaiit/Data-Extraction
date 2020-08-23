cb59645fc3e751ab519844b5b88ef8d490823614
Abdellatif-AitHammadi <abdellatif.aithammadi@gmail.com>
2020-08-23 16:41:09 +0100

    +++

--- youtubeApp/views.py
+++ youtubeApp/views.py
@@ -2,13 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import json
 from django.views.decorators.csrf import csrf_exempt
+
 def index(request):
 	return render(request,"ytindex.html")
 
-
+@csrf_exempt
 def searchv(request):
+	if request.method=='POST':
+		fields=json.loads(request.body)
+		print(fields)
+		return HttpResponse("todo")
 	return render(request,"searchv.html")
 
 
+
+@csrf_exempt
 def ytcomments(request):
+	if request.method=='POST':
+		fields=json.loads(request.body)
+		print(fields)
+		return HttpResponse("todo")
 	return render(request,"ytcomments.html")
