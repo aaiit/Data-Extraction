99a7e5ed4f0c8152996cf08d5651c341c8bb5905
AAyyoouubb <th3assis@gmail.com>
2020-10-07 10:45:29 +0000

    aaaall

--- fire.py
+++ fire.py
@@ -11,40 +11,35 @@ config = {
     "appId": "1:232512095653:web:2180911702dbc68dbd944c",
     "measurementId": "G-4LZS8CR2TY"}
 
+
 firebase = pyrebase.initialize_app(config)
-database = firebase.database()
+database=firebase.database()
 storage = firebase.storage()
 
-
 def loadfile(filename):
-    storage.child("cocooooo/" + filename).download(filename)
-
-
+    storage.child("cocooooo/"+filename).download(filename)
 def savefile(filename):
-    storage.child("cocooooo/" + filename).put(filename)
-
-
+    storage.child("cocooooo/"+filename).put(filename)
 def getrandomid(n=8):
-    A = "AZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbn0123456789"
-    m = len(A)
-    s = ""
+    A="AZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbn0123456789"
+    m=len(A)
+    s=""
     for i in range(n):
-        r = random.randint(0, m - 1)
-        s += A[r]
+        r=random.randint(0,m-1)
+        s+=A[r]
     return s
 
-
-def upload(jsondatastring, type="json"):
-    a = getrandomid()  # str(random.randint(1,19999))
-    database.child("data/%s/" % (type) + a).set(jsondatastring)
-    return a
+def upload(jsondatastring,type="json"):
+	a=getrandomid()#str(random.randint(1,19999))
+	database.child("data/%s/"%(type)+a).set(jsondatastring)
+	return a
 
 
 def uploadimage(imagename):
-    a = getrandomid()  # str(random.randint(1,19999))
-    storage.child("images/%s" % (a)).put(imagename)
-    return storage.child("images/%s" % (a)).get_url("hhh")
+    a=getrandomid()#str(random.randint(1,19999))
+    storage.child("images/%s.svg"%(a)).put(imagename)
+    return storage.child("images/%s.svg"%(a)).get_url("hhh")
 
 
-def uploadfilds(st, a):
-    database.child("data/json/" + a).set(st)
+def uploadfilds(st,a):
+    database.child("data/json/"+a).set(st)
