b8a59eadf6fb2202f1299efc3366ddf27933ab25
AAyyoouubb <th3assis@gmail.com>
2020-10-07 10:34:53 +0000

    aaaall

--- twitterApp/Twitter/Plot.py
+++ twitterApp/Twitter/Plot.py
@@ -1,7 +1,9 @@
-import networkx as nx
-import matplotlib.pyplot as plt
+
 
 def plot_unweighted_graph(graph, filename):
+    import networkx as nx
+    import matplotlib.pyplot as plt
+
     if not isinstance(graph, dict):
         return
     g = nx.Graph()
@@ -10,7 +12,7 @@ def plot_unweighted_graph(graph, filename):
         for end in graph[start]:
             g.add_edge(start.lower(),end)
             g.add_node(end)
-    plt.figure(1, figsize=(g.number_of_nodes()/2, g.number_of_nodes()/2))
-    nx.draw_spring(g,with_labels=True,node_size=200,font_size=6)
+    plt.figure(1, figsize=(g.number_of_nodes()/2+1, g.number_of_nodes()/2+1))
+    nx.draw_spring(g,with_labels=True,node_size=500,font_size=2)
     plt.savefig(filename,format='svg')
     plt.show()
