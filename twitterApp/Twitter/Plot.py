843db94fae04af682c96b6d3644b104844ae2de3
AAyyoouubb <th3assis@gmail.com>
2020-10-07 09:20:41 +0000

    aaaall

--- twitterApp/Twitter/Plot.py
+++ twitterApp/Twitter/Plot.py
@@ -8,9 +8,9 @@ def plot_unweighted_graph(graph, filename):
     for start in graph:
         g.add_node(start)
         for end in graph[start]:
-            g.add_edge(start.lower(),end,color='r')
+            g.add_edge(start.lower(),end)
             g.add_node(end)
     plt.figure(1, figsize=(g.number_of_nodes()/2, g.number_of_nodes()/2))
-    nx.draw_spring(g,with_labels=True,node_size=200,font_size=6,node_color='r')
+    nx.draw_spring(g,with_labels=True,node_size=200,font_size=6)
     plt.savefig(filename,format='svg')
     plt.show()
