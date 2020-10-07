269ab16199684e3a9b960f637176b69961252bf0
AAyyoouubb <th3assis@gmail.com>
2020-10-07 10:50:12 +0000

    aaaall

--- twitterApp/Twitter/Plot.py
+++ twitterApp/Twitter/Plot.py
@@ -15,4 +15,4 @@ def plot_unweighted_graph(graph, filename):
     plt.figure(1, figsize=(g.number_of_nodes()/2+1, g.number_of_nodes()/2+1))
     nx.draw_spring(g,with_labels=True,node_size=500,font_size=2)
     plt.savefig(filename,format='svg')
-    plt.show()
+    plt.close()
