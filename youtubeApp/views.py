3ef5852ab2c9cb300f94a89f820b259c32540e58
Abdellatif-AitHammadi <abdellatif.aithammadi@gmail.com>
2020-10-11 22:24:05 +0100

    corona

--- youtubeApp/views.py
+++ youtubeApp/views.py
@@ -6,18 +6,44 @@ from django.views.decorators.csrf import csrf_exempt
 from fire import upload,uploadfilds
 from datetime import datetime
 from corona import corona
+import pandas as pd
 
 def Corona(request):
-	return HttpResponse(corona())
-	return render(request,"ytindex.html")
+	if "myname" not in request.session:
+		request.session["myname"] = getrandomid(10)
+	# a = id  # request.GET.get("a")
+	# st = database.child("data/json/" + a).get().val()
+	# js = st
+	# if js == None:
+		# return redirect('/')
+	js = corona()#json.loads(js)
+	st=json.dumps(js)
+
+
+	if (js == []): return HttpResponse("empty data")
+	keys = list(js[0].keys())
+
+	def e(l):
+		Q = []
+		for i in l:
+			Q.append(l[i])
+		return Q
+
+	lignes = []
+	for j in js:
+		lignes.append(list(e(j)))
+	return render(request, "table.html", {"CSV":pd.DataFrame(js).to_csv(index=False),"C": keys, "data": lignes, "JSON": st, "id": "corona"})
+
+	# return HttpResponse(corona())
+	# return render(request,"ytindex.html")
 
 def savetohistory(request,h):
-    try:    
-        H=json.loads(request.session["history20"])+[h]
-    except:
-        H=[h]
-    request.session["history20"]=json.dumps(H)
-    print(">>>",request.session["history20"])
+	try:	
+		H=json.loads(request.session["history20"])+[h]
+	except:
+		H=[h]
+	request.session["history20"]=json.dumps(H)
+	print(">>>",request.session["history20"])
 
 def index(request):
 	return render(request,"ytindex.html")
